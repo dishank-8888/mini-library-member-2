28221425a769cd8c68c67f9bf4ec5c1599c67924
pavan4123 <pavan.n@s.amity.edu>
2025-05-26 10:06:03 +0530

    Added DELETE endpoint for book removal and cleanup in app.py

--- app.py
+++ app.py
@@ -43,3 +43,21 @@ def manage_books():
             cover = filename
     books[book_id] = {'id': book_id, 'title': title, 'author': author, 'cover': cover}
     return jsonify(books[book_id]), 201
+    @app.route('/books', methods=['DELETE'])
+def manage_books():
+    book_id = request.args.get('id')
+    if book_id in books:
+        # Delete cover image
+        cover = books[book_id]['cover']
+        if cover:
+            try:
+                os.remove(os.path.join(UPLOAD_FOLDER, cover))
+            except Exception:
+                pass
+        del books[book_id]
+        # Remove transactions for this book
+        for tx in list(transactions):
+            if tx['book_id'] == book_id:
+                transactions.remove(tx)
+        return '', 204
+    return 'Book not found', 404
